a211fcdd3ecf8efc356bdd8264fd945d25cc0f3d
Brad Griffith <griffbrad@gmail.com>
2020-06-30 10:30:33 -0500

    Messing with runtime CSS

--- src/core/css.ts
+++ src/core/css.ts
@@ -8,7 +8,5 @@ export default function css(
   _strings: TemplateStringsArray,
   ..._exprs: Array<string | number | CSSProperties | StyledMeta>
 ): string {
-  throw new Error(
-    'Using the "css" tag in runtime is not supported. Make sure you have set up the Babel plugin correctly.'
-  );
+  console.log(_strings);
 }
